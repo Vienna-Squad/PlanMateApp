e3e3c337ff556a8c12b1ccd45a8ec14aae930f04
a7med naser <ad0112727@gmail.com>
2025-04-28 19:56:31 +0300

    init logic of edit task title use case

--- src/main/kotlin/domain/usecase/task/EditTaskTitleUseCase.kt
+++ src/main/kotlin/domain/usecase/task/EditTaskTitleUseCase.kt
@@ -1,10 +1,14 @@
 package org.example.domain.usecase.task
 
-import org.example.domain.entity.Task
+import org.example.domain.NoTaskFoundException
 import org.example.domain.repository.TasksRepository
 
 class EditTaskTitleUseCase (
     private val tasksRepository: TasksRepository
 ) {
-    operator fun invoke(taskId: String, title: String) {}
+    operator fun invoke(taskId: String, title: String) {
+         // get Tasks from tasksRepo
+         // check if task is found
+        return throw NoTaskFoundException("")
+    }
 }
