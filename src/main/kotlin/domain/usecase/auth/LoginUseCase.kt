5e16999c33c298f910c062fb6b633279e7a5a4c4
a7med naser <ad0112727@gmail.com>
2025-04-28 19:58:47 +0300

    init logic of login use case

--- src/main/kotlin/domain/usecase/auth/LoginUseCase.kt
+++ src/main/kotlin/domain/usecase/auth/LoginUseCase.kt
@@ -1,12 +1,18 @@
 package org.example.domain.usecase.auth
 
+import org.example.domain.LoginException
 import org.example.domain.entity.User
 import org.example.domain.repository.AuthenticationRepository
 
 class LoginUseCase(
     private val authenticationRepository: AuthenticationRepository
+    // storage
 ) {
     operator fun invoke(username: String, password: String): Result<User> {
-        return Result.failure(Exception())
+        // get users list to check
+        // is user found in storage
+        return Result.failure(LoginException())
     }
 }
+
+const val LOGIN_EXCEPTION_MESSAGE = "The user name or password you entered isn't found in storage"
