0909c8110d584858561d527a67919436b646a1c0
a7med naser <ad0112727@gmail.com>
2025-04-28 19:58:37 +0300

    init logic of register use case

--- src/main/kotlin/domain/usecase/auth/RegisterUserUseCase.kt
+++ src/main/kotlin/domain/usecase/auth/RegisterUserUseCase.kt
@@ -1,10 +1,16 @@
 package org.example.domain.usecase.auth
 
+import org.example.domain.RegisterException
 import org.example.domain.entity.UserType
 import org.example.domain.repository.AuthenticationRepository
 
 class RegisterUserUseCase(
     private val authenticationRepository: AuthenticationRepository
 ) {
-    operator fun invoke(username: String, password: String, role: UserType) {}
+    operator fun invoke(username: String, password: String, role: UserType) {
+          // is username & password valid
+          //  check role
+          // is user is found in data
+          return throw RegisterException()
+    }
 }
