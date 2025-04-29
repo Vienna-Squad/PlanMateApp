bd1e0a70082ebea15c719a8d9f733deab240462b
Mohannad Ahmed <mohannad199344@gmail.com>
2025-04-30 00:54:32 +0300

    add the implementation of EditProjectNameUseCase to pass all its testcases with 100% coverage

--- src/main/kotlin/domain/usecase/project/EditProjectNameUseCase.kt
+++ src/main/kotlin/domain/usecase/project/EditProjectNameUseCase.kt
@@ -1,9 +1,54 @@
 package org.example.domain.usecase.project
 
+import org.example.domain.AccessDeniedException
+import org.example.domain.InvalidProjectIdException
+import org.example.domain.NoProjectFoundException
+import org.example.domain.UnauthorizedException
+import org.example.domain.entity.ChangedLog
+import org.example.domain.entity.Log
+import org.example.domain.entity.Project
+import org.example.domain.entity.User
+import org.example.domain.entity.UserType
+import org.example.domain.repository.AuthenticationRepository
+import org.example.domain.repository.LogsRepository
 import org.example.domain.repository.ProjectsRepository
 
 class EditProjectNameUseCase(
-    private val projectsRepository: ProjectsRepository
+    private val projectsRepository: ProjectsRepository,
+    private val logsRepository: LogsRepository,
+    private val authenticationRepository: AuthenticationRepository
 ) {
-    operator fun invoke(projectId: String, name: String) {}
+
+    operator fun invoke(projectId: String, name: String) {
+        doIfAuthorized(authenticationRepository::getCurrentUser) { user ->
+            if (user.type == UserType.MATE) throw AccessDeniedException()
+            doIfExistedProject(projectId, projectsRepository::get) { project ->
+                if (project.createdBy != user.id) throw AccessDeniedException()
+                if (name != project.name) {
+                    projectsRepository.update(project.copy(name = name))
+                    logsRepository.add(
+                        ChangedLog(
+                            username = user.username,
+                            affectedId = projectId,
+                            affectedType = Log.AffectedType.PROJECT,
+                            changedFrom = project.name,
+                            changedTo = name,
+                        )
+                    )
+                }
+            }
+        }
+    }
+
+    private fun doIfAuthorized(getCurrentUser: () -> Result<User>, block: (User) -> Unit) {
+        block(getCurrentUser().getOrElse { throw UnauthorizedException() })
+    }
+
+    private fun doIfExistedProject(
+        projectId: String,
+        getProject: (String) -> Result<Project>,
+        block: (Project) -> Unit
+    ) {
+        block(getProject(projectId).getOrElse { throw if (projectId.isBlank()) InvalidProjectIdException() else NoProjectFoundException() })
+    }
 }
