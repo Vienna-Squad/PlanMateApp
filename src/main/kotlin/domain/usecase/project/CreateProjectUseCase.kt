637bbd790480686c65ee79bd9909f43446e786d2
nada <nadamansoursamir@gmail.com>
2025-04-29 18:34:25 +0300

    complete the implementation of CreateProjectUseCase

--- src/main/kotlin/domain/usecase/project/CreateProjectUseCase.kt
+++ src/main/kotlin/domain/usecase/project/CreateProjectUseCase.kt
@@ -1,4 +1,8 @@
 package org.example.domain.usecase.project
+
+import org.example.domain.FailedToAddProjectException
+import org.example.domain.UnauthorizedException
+import org.example.domain.entity.*
 import org.example.domain.repository.AuthenticationRepository
 import org.example.domain.repository.LogsRepository
 import org.example.domain.repository.ProjectsRepository
@@ -9,11 +13,24 @@ class CreateProjectUseCase(
     private val authenticationRepository: AuthenticationRepository,
     private val logsRepository: LogsRepository
 ) {
-    operator fun invoke(
-        name: String,
-        states: List<String>,
-        creatorId: String,
-        matesIds: List<String> = emptyList()
-    ) {
+    operator fun invoke(name: String, states: List<String>, creatorId: String, matesIds: List<String> = emptyList()) {
+
+        val currentUser = authenticationRepository.getCurrentUser().getOrElse { throw UnauthorizedException() }
+
+        if (currentUser.type != UserType.ADMIN) {
+            throw UnauthorizedException()
+        }
+
+        val newProject = Project(name, states, creatorId, matesIds)
+        projectsRepository.add(newProject).getOrElse { throw FailedToAddProjectException() }
+
+        logsRepository.add(
+            log = CreatedLog(
+                username = currentUser.username,
+                affectedType = Log.AffectedType.PROJECT,
+                affectedId = newProject.id
+            )
+        ).getOrElse { throw FailedToAddProjectException() }
+
     }
 }
