14503a33d020e1d3b1c012dc7d08ec39cda6a11c
a7med naser <ad0112727@gmail.com>
2025-04-29 19:44:39 +0300

    refactor AuthenticationRepository create user

--- src/main/kotlin/domain/repository/AuthenticationRepository.kt
+++ src/main/kotlin/domain/repository/AuthenticationRepository.kt
@@ -4,8 +4,7 @@ import org.example.domain.entity.User
 
 interface AuthenticationRepository {
     fun getAllUsers(): Result<List<User>>
-    fun createUser(user: User): Result<Unit>
+    fun createUser(user: User)
     fun getCurrentUser(): Result<User>
     fun getUser(userId: String): Result<User>
-
 }
