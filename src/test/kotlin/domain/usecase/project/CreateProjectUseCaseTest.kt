98a4ade85935893a0258b6478ec200bdd2781b0d
nada <nadamansoursamir@gmail.com>
2025-04-29 07:29:12 +0300

    add FailedToAddProjectException

--- src/test/kotlin/domain/usecase/project/CreateProjectUseCaseTest.kt
+++ src/test/kotlin/domain/usecase/project/CreateProjectUseCaseTest.kt
@@ -3,6 +3,7 @@ package domain.usecase.project
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.example.domain.FailedToAddProjectException
 import org.example.domain.entity.Project
 import org.example.domain.repository.ProjectsRepository
 import org.example.domain.usecase.project.CreateProjectUseCase
@@ -13,7 +14,7 @@ import org.junit.jupiter.api.assertThrows
 class CreateProjectUseCaseTest {
 
 
-    lateinit var prjectRepository: ProjectsRepository
+    lateinit var projectRepository: ProjectsRepository
     lateinit var createProjectUseCase: CreateProjectUseCase
 
     val name = "graduation project"
@@ -25,34 +26,32 @@ class CreateProjectUseCaseTest {
     @BeforeEach
     fun setUp() {
 
-        prjectRepository = mockk()
-        createProjectUseCase = CreateProjectUseCase(prjectRepository)
+        projectRepository = mockk()
+        createProjectUseCase = CreateProjectUseCase(projectRepository)
 
     }
 
 
     @Test
     fun `should create project and save it when valid data is provided`() {
-
         //given
-
-        every { prjectRepository.addProject(newProject) } returns Unit
+        every { projectRepository.addProject(newProject) } returns Unit
 
         // when
         createProjectUseCase(name, states, createdBy, matesIds)
 
         // then
-        verify(exactly = 1) { prjectRepository.addProject(newProject) }
+        verify(exactly = 1) { projectRepository.addProject(newProject) }
 
     }
 
     @Test
-    fun `should throw exception when project not added`() {
+    fun `should throw FailedToAddProjectException when project not added`() {
         //given
-        every { prjectRepository.addProject(any()) } throws Exception("failed to add project")
+        every { projectRepository.addProject(any()) } throws FailedToAddProjectException()
 
         //when & then
-        assertThrows<Exception> {
+        assertThrows<FailedToAddProjectException> {
             createProjectUseCase(name, states, createdBy, matesIds)
         }
 
