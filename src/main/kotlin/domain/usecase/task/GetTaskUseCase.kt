7d7e36ab2280a105eb5a44433f4f7a14d8cf7929
Mostafa Ibrahim <mostafa1996998@gmail.com>
2025-04-30 15:18:56 +0300

    Implement GetTaskUseCase to pass all tests

--- src/main/kotlin/domain/usecase/task/GetTaskUseCase.kt
+++ src/main/kotlin/domain/usecase/task/GetTaskUseCase.kt
@@ -1,6 +1,12 @@
 package org.example.domain.usecase.task
 
+import org.example.domain.AccessDeniedException
+import org.example.domain.InvalidIdException
+import org.example.domain.NoFoundException
+import org.example.domain.UnauthorizedException
 import org.example.domain.entity.Task
+import org.example.domain.entity.User
+import org.example.domain.entity.UserType
 import org.example.domain.repository.AuthenticationRepository
 import org.example.domain.repository.TasksRepository
 
@@ -11,6 +17,28 @@ class GetTaskUseCase(
 
     operator fun invoke(taskId: String): Task {
 
-        TODO("Not yet implemented")
+        validateInputs(taskId)
+
+        val userResult = authenticationRepository.getCurrentUser()
+        if (userResult.isFailure) {
+            throw UnauthorizedException()
+        }
+        val user = userResult.getOrThrow()
+        validateUserAuthorization(user)
+
+
+        val taskResult = tasksRepository.get(taskId)
+        if (taskResult.isFailure) {
+            throw NoFoundException()
+        }
+        return taskResult.getOrThrow()
     }
-}
+
+    private fun validateInputs(taskId: String) {
+        require(taskId.isNotBlank()) { throw InvalidIdException() }
+    }
+
+    private fun validateUserAuthorization(user: User) {
+        require(user.type != UserType.MATE) { throw AccessDeniedException() }
+    }
+    }
