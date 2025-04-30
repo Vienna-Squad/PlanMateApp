ad6ccdb75c943fee2e000fa0919e91c75656ec69
Mohannad Ahmed <mohannad199344@gmail.com>
2025-04-30 20:54:59 +0300

    edit LogsCsvStorage to implement CsvStorage

--- src/main/kotlin/data/storage/LogsCsvStorage.kt
+++ src/main/kotlin/data/storage/LogsCsvStorage.kt
@@ -1,39 +1,118 @@
 package org.example.data.storage
 
-import data.storage.Storage
 import org.example.domain.entity.*
+import org.example.domain.entity.Log.ActionType
+import org.example.domain.entity.Log.AffectedType
 import java.io.File
 import java.io.FileNotFoundException
 import java.text.ParseException
+import java.time.LocalDateTime
 
-class LogsCsvStorage(private val file: File = File(FILE_NAME)) : Storage<Log> {
+class LogsCsvStorage(private val file: File = File(FILE_NAME)) : CsvStorageDemo<Log> {
     //[ActionType,username, affectedId, affectedType, dateTime,changedFrom, changedTo]
     override fun read(): List<Log> {
         if (!file.exists()) throw FileNotFoundException()
-        return file.readLines().map { row -> parseToLog(row.split(",")) }
+        return file.readLines().map { row -> fromCsvRow(row.split(",")) }
     }
 
     override fun append(item: Log) {
         if (!file.exists()) file.createNewFile()
-        file.appendText(item.toCsvRow().joinToString(",") + "\n")
+        file.appendText(toCsvRow(item))
     }
 
-    private fun parseToLog(fields: List<String>): Log {
+    override fun toCsvRow(item: Log): String {
+        return when (item) {
+            is AddedLog -> listOf(
+                ActionType.CHANGED.name,
+                item.username,
+                item.affectedId,
+                item.affectedType,
+                item.dateTime,
+                "",
+                item.addedTo
+            )
+
+            is ChangedLog -> listOf(
+                ActionType.CHANGED.name,
+                item.username,
+                item.affectedId,
+                item.affectedType,
+                item.dateTime,
+                item.changedFrom,
+                item.changedTo
+            )
+
+            is CreatedLog -> listOf(
+                ActionType.CREATED.name,
+                item.username,
+                item.affectedId,
+                item.affectedType,
+                item.dateTime,
+                "",
+                ""
+            )
+
+            is DeletedLog -> listOf(
+                ActionType.DELETED.name,
+                item.username,
+                item.affectedId,
+                item.affectedType,
+                item.dateTime,
+                item.deletedFrom ?: "",
+                ""
+            )
+        }.joinToString(",") + "\n"
+    }
+
+    override fun fromCsvRow(fields: List<String>): Log {
         //[ActionType,username, affectedId, affectedType, dateTime,changedFrom, changedTo]
         if (fields.size != 7) throw ParseException("wrong size of fields it is: ${fields.size}", 0)
-        val actionType = Log.ActionType.entries.firstOrNull { it.name == fields.first() } ?: throw ParseException(
+        val actionType = ActionType.entries.firstOrNull { it.name == fields.first() } ?: throw ParseException(
             fields.first(),
             0
         )
         return when (actionType) {
-            Log.ActionType.CHANGED -> ChangedLog(fields)
-            Log.ActionType.ADDED -> AddedLog(fields)
-            Log.ActionType.DELETED -> DeletedLog(fields)
-            Log.ActionType.CREATED -> CreatedLog(fields)
+            ActionType.CHANGED -> ChangedLog(
+                username = fields[USERNAME_INDEX],
+                affectedId = fields[AFFECTED_ID_INDEX],
+                affectedType = AffectedType.valueOf(fields[AFFECTED_TYPE_INDEX]),
+                dateTime = LocalDateTime.parse(fields[DATE_TIME_INDEX]),
+                changedFrom = fields[FROM_INDEX],
+                changedTo = fields[TO_INDEX]
+            )
+
+            ActionType.ADDED -> AddedLog(
+                username = fields[USERNAME_INDEX],
+                affectedId = fields[AFFECTED_ID_INDEX],
+                affectedType = AffectedType.valueOf(fields[AFFECTED_TYPE_INDEX]),
+                dateTime = LocalDateTime.parse(fields[DATE_TIME_INDEX]),
+                addedTo = fields[TO_INDEX]
+            )
+
+            ActionType.DELETED -> DeletedLog(
+                username = fields[USERNAME_INDEX],
+                affectedId = fields[AFFECTED_ID_INDEX],
+                affectedType = AffectedType.valueOf(fields[AFFECTED_TYPE_INDEX]),
+                dateTime = LocalDateTime.parse(fields[DATE_TIME_INDEX]),
+                deletedFrom = fields[FROM_INDEX],
+            )
+
+            ActionType.CREATED -> CreatedLog(
+                username = fields[USERNAME_INDEX],
+                affectedId = fields[AFFECTED_ID_INDEX],
+                affectedType = AffectedType.valueOf(fields[AFFECTED_TYPE_INDEX]),
+                dateTime = LocalDateTime.parse(fields[DATE_TIME_INDEX]),
+            )
         }
     }
 
     companion object {
         const val FILE_NAME = "logs.csv"
+        const val USERNAME_INDEX = 1
+        const val AFFECTED_ID_INDEX = 2
+        const val AFFECTED_TYPE_INDEX = 3
+        const val DATE_TIME_INDEX = 4
+        const val FROM_INDEX = 5
+        const val TO_INDEX = 6
     }
 }
