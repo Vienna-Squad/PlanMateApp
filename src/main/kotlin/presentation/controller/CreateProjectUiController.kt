4c349b6a7c8f1587f0018a19374487b9fd132e41
nada <nadamansoursamir@gmail.com>
2025-05-01 22:15:23 +0300

    refactor GetProjectUiController enhancement

--- src/main/kotlin/presentation/controller/CreateProjectUiController.kt
+++ src/main/kotlin/presentation/controller/CreateProjectUiController.kt
@@ -4,11 +4,14 @@ import org.example.domain.InvalidIdException
 import org.example.domain.usecase.project.CreateProjectUseCase
 import org.example.presentation.utils.interactor.Interactor
 import org.example.presentation.utils.interactor.StringInteractor
+import org.example.presentation.utils.viewer.ItemViewer
+import org.example.presentation.utils.viewer.StringViewer
 import org.koin.java.KoinJavaComponent.getKoin
 
 class CreateProjectUiController(
     private val createProjectUseCase: CreateProjectUseCase = getKoin().get(),
     private val stringInteractor: Interactor<String> = StringInteractor(),
+    private val itemViewer: ItemViewer<String> = StringViewer()
 ) : UiController {
     override fun execute() {
         tryAndShowError {
@@ -28,7 +31,7 @@ class CreateProjectUiController(
             val matesId = matesIdInput.split(",").map { it.trim() }
 
             createProjectUseCase(name = name, states = states, creatorId = creatorId, matesIds = matesId)
-            println("Project created successfully")
+            itemViewer.view("Project created successfully")
 
         }
     }
