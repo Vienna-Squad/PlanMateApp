e9bb27103ad4348d5ad3ec4b926690b02c084fa1
nada <nadamansoursamir@gmail.com>
2025-05-02 01:22:41 +0300

    add validation in GetProjectHistoryUiController

--- src/main/kotlin/presentation/controller/GetProjectHistoryUiController.kt
+++ src/main/kotlin/presentation/controller/GetProjectHistoryUiController.kt
@@ -1,5 +1,6 @@
 package org.example.presentation.controller
 
+import org.example.domain.InvalidIdException
 import org.example.domain.entity.Log
 import org.example.domain.usecase.project.GetProjectHistoryUseCase
 import org.example.presentation.utils.interactor.Interactor
@@ -8,9 +9,9 @@ import org.example.presentation.utils.viewer.ItemsViewer
 import org.koin.java.KoinJavaComponent.getKoin
 
 class GetProjectHistoryUiController(
-    private val getProjectHistoryUseCase: GetProjectHistoryUseCase=getKoin().get(),
-    private val stringInteractor:Interactor<String> = StringInteractor(),
-    private val itemsViewer: ItemsViewer<Log>
+    private val getProjectHistoryUseCase: GetProjectHistoryUseCase = getKoin().get(),
+    private val stringInteractor: Interactor<String> = StringInteractor(),
+//    private val itemsViewer: ItemsViewer<Log>
 
 ) : UiController {
 
@@ -19,13 +20,19 @@ class GetProjectHistoryUiController(
 
             println("enter your project id: ")
             val projectId = stringInteractor.getInput()
+            if (projectId.isEmpty()) throw InvalidIdException()
 
             val projectHistory = getProjectHistoryUseCase(projectId = projectId)
             if (projectHistory.isEmpty()) {
                 println("No logs found for this project.")
             } else {
                 println("Project History Logs:")
-                itemsViewer.view(projectHistory)
+//                itemsViewer.view(projectHistory)
+                projectHistory.forEach { log ->
+                    println(log)
+                }
+
+
             }
 
 
