e9946cfd1e36aa073333304fa5ff1b58b5af14ab
nada <nadamansoursamir@gmail.com>
2025-05-02 01:16:37 +0300

    add validation in DeleteMateFromTaskUiController

--- src/main/kotlin/presentation/controller/DeleteMateFromTaskUiController.kt
+++ src/main/kotlin/presentation/controller/DeleteMateFromTaskUiController.kt
@@ -1,5 +1,6 @@
 package org.example.presentation.controller
 
+import org.example.domain.InvalidIdException
 import org.example.domain.usecase.task.DeleteMateFromTaskUseCase
 import org.example.presentation.utils.interactor.Interactor
 import org.example.presentation.utils.interactor.StringInteractor
@@ -20,9 +21,11 @@ class DeleteMateFromTaskUiController(
 
             println("enter your task id: ")
             val taskId = stringInteractor.getInput()
+            if(taskId.isEmpty())throw InvalidIdException()
 
             println("enter your mate id to remove: ")
             val mateId = stringInteractor.getInput()
+            if(mateId.isEmpty())throw InvalidIdException()
 
             deleteMateFromTaskUseCase(taskId = taskId, mate = mateId)
             itemViewer.view("mate deleted from task successfully")
