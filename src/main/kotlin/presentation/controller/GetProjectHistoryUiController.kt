73b4b619fae9b0a29c3af8ffd40700f71fc9e808
nada <nadamansoursamir@gmail.com>
2025-05-01 21:07:46 +0300

    refactor GetProjectHistoryUiController to use itemsViewer to view project history

--- src/main/kotlin/presentation/controller/GetProjectHistoryUiController.kt
+++ src/main/kotlin/presentation/controller/GetProjectHistoryUiController.kt
@@ -1,13 +1,16 @@
 package org.example.presentation.controller
 
+import org.example.domain.entity.Log
 import org.example.domain.usecase.project.GetProjectHistoryUseCase
 import org.example.presentation.utils.interactor.Interactor
 import org.example.presentation.utils.interactor.StringInteractor
+import org.example.presentation.utils.viewer.ItemsViewer
 import org.koin.java.KoinJavaComponent.getKoin
 
 class GetProjectHistoryUiController(
     private val getProjectHistoryUseCase: GetProjectHistoryUseCase=getKoin().get(),
-    private val stringInteractor:Interactor<String> = StringInteractor()
+    private val stringInteractor:Interactor<String> = StringInteractor(),
+    private val itemsViewer: ItemsViewer<Log>
 
 ) : UiController {
 
@@ -22,9 +25,7 @@ class GetProjectHistoryUiController(
                 println("No logs found for this project.")
             } else {
                 println("Project History Logs:")
-                projectHistory.forEach {
-                    println(it)
-                }
+                itemsViewer.view(projectHistory)
             }
 
 
