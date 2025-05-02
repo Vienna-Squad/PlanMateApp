fa88114a232c0a572c74c968575a47e57835c965
abdo-essam <abdo.e.mosa@gmail.com>
2025-05-02 18:53:42 +0300

    feat: Implement DeleteTaskUseCase and add corresponding unit tests

--- src/main/kotlin/domain/usecase/task/DeleteTaskUseCase.kt
+++ src/main/kotlin/domain/usecase/task/DeleteTaskUseCase.kt
@@ -1,9 +1,13 @@
 package org.example.domain.usecase.task
 
+import org.example.domain.FailedToAddException
 import org.example.domain.repository.TasksRepository
 
 class DeleteTaskUseCase(
     private val tasksRepository: TasksRepository
 ) {
-    operator fun invoke(taskId: String) {}
+    operator fun invoke(taskId: String) {
+        tasksRepository.delete(taskId)
+            .getOrElse { throw FailedToAddException() }
+    }
 }
