bf02d7c173c21a8406972ce2ed34a71b49451568
Mohannad Ahmed <mohannad199344@gmail.com>
2025-05-07 20:12:10 +0300

    add test cases to GetAllProjectsUseCase and handle it

--- src/main/kotlin/domain/usecase/project/GetAllProjectsUseCase.kt
+++ src/main/kotlin/domain/usecase/project/GetAllProjectsUseCase.kt
@@ -1,6 +1,6 @@
 package org.example.domain.usecase.project
 
-import org.example.domain.UnauthorizedException
+import org.example.domain.NotFoundException
 import org.example.domain.repository.ProjectsRepository
 import org.example.domain.repository.UsersRepository
 
@@ -9,8 +9,8 @@ class GetAllProjectsUseCase(
     private val usersRepository: UsersRepository,
 ) {
     operator fun invoke() = projectsRepository.getAllProjects().let { projects ->
-        usersRepository.getCurrentUser()?.let { currentUser ->
-            projects.filter { it.createdBy == currentUser.id }
-        } ?: throw UnauthorizedException()
+        usersRepository.getCurrentUser().let { currentUser ->
+            projects.filter { it.createdBy == currentUser.id }.ifEmpty { throw NotFoundException("projects") }
+        }
     }
 }
