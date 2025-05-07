b00a4493f2549f1008256f03d0233277895f78ef
Mohannad Ahmed <mohannad199344@gmail.com>
2025-05-07 07:44:18 +0300

    handle the accessibility of mate getProjectById

--- src/main/kotlin/data/repository/ProjectsRepositoryImpl.kt
+++ src/main/kotlin/data/repository/ProjectsRepositoryImpl.kt
@@ -1,12 +1,10 @@
 package org.example.data.repository
 
 import org.example.data.datasource.local.LocalDataSource
-import org.example.data.datasource.local.preferences.Preference
 import org.example.data.datasource.remote.RemoteDataSource
 import org.example.data.utils.authSafeCall
 import org.example.data.utils.safeCall
 import org.example.domain.AccessDeniedException
-import org.example.domain.NotFoundException
 import org.example.domain.entity.Project
 import org.example.domain.entity.UserRole
 import org.example.domain.repository.ProjectsRepository
@@ -16,18 +14,17 @@ import java.util.*
 class ProjectsRepositoryImpl(
     private val projectsRemoteDataSource: RemoteDataSource<Project>,
     private val projectsLocalDataSource: LocalDataSource<Project>,
-    private val preferences: Preference
 ) : ProjectsRepository {
 
     override fun getProjectById(projectId: UUID) = authSafeCall { currentUser ->
         projectsRemoteDataSource.getById(projectId).let { project ->
-            if (project.createdBy != currentUser.id) throw AccessDeniedException()
+            if (project.createdBy != currentUser.id || currentUser.id !in  project.matesIds) throw AccessDeniedException()
             project
         }
     }
 
     override fun getAllProjects() = safeCall {
-        projectsRemoteDataSource.getAll().ifEmpty { throw NotFoundException("projects") }
+        projectsRemoteDataSource.getAll()
     }
 
     override fun updateProject(updatedProject: Project) = authSafeCall { currentUser ->
