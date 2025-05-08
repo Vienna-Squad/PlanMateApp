a2cdb4cf3c57982e3e2779dc72425b0ecec1c14e
mohamedshemees <mohamedessampd@gmail.com>
2025-05-08 12:57:55 +0300

    test: enhance EditTaskStateUseCaseTest with user repository integration and improved exception handling

--- src/test/kotlin/domain/usecase/task/EditTaskStateUseCaseTest.kt
+++ src/test/kotlin/domain/usecase/task/EditTaskStateUseCaseTest.kt
@@ -1,31 +1,31 @@
 package domain.usecase.task
 
+import dummyTasks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.example.domain.entity.ChangedLog
-import org.example.domain.entity.Task
 import org.example.domain.repository.LogsRepository
 import org.example.domain.repository.TasksRepository
+import org.example.domain.repository.UsersRepository
 import org.example.domain.usecase.task.EditTaskStateUseCase
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import java.time.LocalDateTime
-import java.util.*
 
 class EditTaskStateUseCaseTest {
     private lateinit var editTaskStateUseCase: EditTaskStateUseCase
     private val logsRepository: LogsRepository = mockk(relaxed = true)
-
+    private val usersRepository: UsersRepository = mockk(relaxed = true)
     private val tasksRepository: TasksRepository = mockk(relaxed = true)
-
+    private val dummyTask = dummyTasks[0]
 
     @BeforeEach
     fun setup() {
         editTaskStateUseCase = EditTaskStateUseCase(
             tasksRepository,
-            logsRepository
+            logsRepository,
+            usersRepository
         )
     }
 
@@ -44,53 +44,56 @@ class EditTaskStateUseCaseTest {
             })
         }
         verify {
-            logsRepository.addLog(match
-            {
-                it is ChangedLog
-            })
+            logsRepository.addLog(
+                match
+                {
+                    it is ChangedLog
+                })
         }
     }
+
     @Test
     fun `should throw an Exception and not log when getTaskById fails `() {
         // Given
+
         every { tasksRepository.getTaskById(dummyTask.id) } throws Exception()
 
         // when&Then
         assertThrows<Exception> {
             editTaskStateUseCase(dummyTask.id, "In Progress")
         }
-        verify (exactly = 0 ){
-            logsRepository.addLog(match
-            {
-                it is ChangedLog
+        verify(exactly = 0) {
+            tasksRepository.updateTask(match {
+                it.id == dummyTask.id
             })
         }
+        verify(exactly = 0) {
+            logsRepository.addLog(
+                match
+                {
+                    it is ChangedLog
+                })
+        }
     }
+
     @Test
     fun `should throw an Exception and not log when updateTask fails `() {
         // Given
-        every { tasksRepository.updateTask(any()) }throws Exception()
+        every { tasksRepository.updateTask(any()) } throws Exception()
         // when&Then
         assertThrows<Exception> {
             editTaskStateUseCase(dummyTask.id, "In Progress")
         }
-        verify (exactly = 0 ){
-            logsRepository.addLog(match
-            {
-                it is ChangedLog
-            })
+
+        verify(exactly = 0) {
+            logsRepository.addLog(
+                match
+                {
+                    it is ChangedLog
+                })
         }
     }
 
 
 }
 
-private val dummyTask = Task(
-    id = UUID.randomUUID(),
-    title = "Sample Task",
-    state = "To Do",
-    assignedTo = listOf(UUID.randomUUID(), UUID.randomUUID()),
-    createdBy = UUID.randomUUID(),
-    createdAt = LocalDateTime.now(),
-    projectId = UUID.randomUUID()
-)
