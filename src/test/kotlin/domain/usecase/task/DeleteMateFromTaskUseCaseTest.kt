1cd692386cf7712b325c822878f87ed9904557a3
mohamedshemees <mohamedessampd@gmail.com>
2025-05-08 11:54:27 +0300

    test: enhance DeleteMateFromTaskUseCaseTest with user logging and exception handling

--- src/test/kotlin/domain/usecase/task/DeleteMateFromTaskUseCaseTest.kt
+++ src/test/kotlin/domain/usecase/task/DeleteMateFromTaskUseCaseTest.kt
@@ -5,8 +5,10 @@ import dummyTasks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.example.domain.entity.DeletedLog
 import org.example.domain.repository.LogsRepository
 import org.example.domain.repository.TasksRepository
+import org.example.domain.repository.UsersRepository
 import org.example.domain.usecase.task.DeleteMateFromTaskUseCase
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -14,53 +16,61 @@ import org.junit.jupiter.api.assertThrows
 
 class DeleteMateFromTaskUseCaseTest {
 
-    lateinit var tasksRepository: TasksRepository
-    lateinit var deleteMateFromTaskUseCase: DeleteMateFromTaskUseCase
-    lateinit var logsRepository: LogsRepository
 
+    lateinit var deleteMateFromTaskUseCase: DeleteMateFromTaskUseCase
+    private val tasksRepository: TasksRepository = mockk(relaxed = true)
+    private val logsRepository: LogsRepository = mockk(relaxed = true)
+    private val usersRepository: UsersRepository = mockk(relaxed = true)
+
+    private val dummyTask = dummyTasks[0]
     @BeforeEach
     fun setUp() {
-        tasksRepository = mockk(relaxed = true)
-        logsRepository = mockk(relaxed = true)
-        deleteMateFromTaskUseCase = DeleteMateFromTaskUseCase(tasksRepository, logsRepository)
+        deleteMateFromTaskUseCase = DeleteMateFromTaskUseCase(tasksRepository, logsRepository, usersRepository  )
+
     }
 
     @Test
     fun `should delete mate when given task id and mate id`() {
         //Given
-        every { tasksRepository.getTaskById(dummyTasks[0].id) } returns dummyTasks[0]
+        val dummyMates = dummyTask.assignedTo
+        every { tasksRepository.getTaskById(dummyTask.id) } returns dummyTask
         // When
-        deleteMateFromTaskUseCase(dummyTasks[0].id, dummyTasks[0].assignedTo[0])
+        deleteMateFromTaskUseCase(dummyTask.id, dummyMates[0])
         //Then
         verify {
             tasksRepository.updateTask(match {
                 !
-                (it.assignedTo.contains(dummyTasks[0].assignedTo[0]))
+                (it.assignedTo.contains(dummyMates[0]))
             })
         }
+        verify { logsRepository.addLog(match { it is DeletedLog }) }
     }
 
     @Test
     fun `should throw Exception when tasksRepository getTaskById throw Exception given task id`() {
         //Given
-        every { tasksRepository.getTaskById(dummyTasks[0].id) } throws Exception()
+        every { tasksRepository.getTaskById(dummyTask.id) } throws Exception()
 
         // When & Then
         assertThrows<Exception> {
-            deleteMateFromTaskUseCase(dummyTasks[0].id, dummyMate.id)
+            deleteMateFromTaskUseCase(dummyTask.id, dummyMate.id)
         }
+        verify(exactly = 0) { logsRepository.addLog(match { it is DeletedLog }) }
 
     }
 
     @Test
     fun `should throw Exception when tasksRepository updateTask throw Exception given task id`() {
         //Given
+
+        every { tasksRepository.updateTask(any()) } throws Exception()
         every { tasksRepository.updateTask(any()) } throws Exception()
 
         // When & Then
         assertThrows<Exception> {
-            deleteMateFromTaskUseCase(dummyTasks[0].id, dummyMate.id)
+            deleteMateFromTaskUseCase(dummyTask.id, dummyMate.id)
         }
+        verify (exactly = 0){ logsRepository.addLog(match { it is DeletedLog }) }
 
     }
 
@@ -71,7 +81,7 @@ class DeleteMateFromTaskUseCaseTest {
 
         // When & Then
         assertThrows<Exception> {
-            deleteMateFromTaskUseCase(dummyTasks[0].id, dummyMate.id)
+            deleteMateFromTaskUseCase(dummyTask.id, dummyMate.id)
         }
 
     }
