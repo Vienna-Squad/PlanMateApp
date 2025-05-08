35711cdad144a6948ad9a7b9ff3fc7b154424710
mohamedshemees <mohamedessampd@gmail.com>
2025-05-08 12:42:43 +0300

    test: update DeleteTaskUseCaseTest to use dummy tasks and improve logging verification

--- src/test/kotlin/domain/usecase/task/DeleteTaskUseCaseTest.kt
+++ src/test/kotlin/domain/usecase/task/DeleteTaskUseCaseTest.kt
@@ -1,54 +1,51 @@
 package domain.usecase.task
 
-import io.mockk.every
-import io.mockk.mockk
-import io.mockk.verify
+import dummyTasks
+import io.mockk.*
+import org.example.domain.entity.DeletedLog
 import org.example.domain.entity.Log
-import org.example.domain.entity.Task
-import org.example.domain.entity.User
-import org.example.domain.entity.UserRole
 import org.example.domain.repository.LogsRepository
 import org.example.domain.repository.TasksRepository
+import org.example.domain.repository.UsersRepository
 import org.example.domain.usecase.task.DeleteTaskUseCase
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
-import java.time.LocalDateTime
-import java.util.*
 
 class DeleteTaskUseCaseTest {
-    private lateinit var tasksRepository: TasksRepository
-    private lateinit var logsRepository: LogsRepository
-
+    private val tasksRepository: TasksRepository = mockk(relaxed = true)
+    private val logsRepository: LogsRepository = mockk(relaxed = true)
+    private val usersRepository: UsersRepository = mockk(relaxed = true)
     private lateinit var deleteTaskUseCase: DeleteTaskUseCase
-
+    private val dummyTask = dummyTasks[0]
 
     @BeforeEach
     fun setUp() {
-        tasksRepository = mockk(relaxed = true)
-        logsRepository = mockk(relaxed = true)
         deleteTaskUseCase = DeleteTaskUseCase(
             tasksRepository,
             logsRepository,
+            usersRepository
         )
     }
 
     @Test
-    fun `should delete project and add log when task exists`() {
+    fun `should delete task and add log when task exists`() {
         // Given
-        every { tasksRepository.deleteTaskById(task.id) } returns Unit
+
+        every { tasksRepository.deleteTaskById(dummyTask.id) } just Runs
 
         // When
-        deleteTaskUseCase(task.id)
+        deleteTaskUseCase(dummyTask.id)
         // Then
         verify {
             tasksRepository.deleteTaskById(match {
-                it == task.id
+                it == dummyTask.id
             })
         }
         verify {
             logsRepository.addLog(match {
-                it.affectedId == task.id.toString() &&
+                it is DeletedLog &&
+                        it.affectedId == dummyTask.id.toString() &&
                         it.affectedType == Log.AffectedType.TASK
 
             })
@@ -59,45 +56,24 @@ class DeleteTaskUseCaseTest {
     @Test
     fun `should not log if task deletion fails`() {
         // Given
-        every { tasksRepository.deleteTaskById(task.id) } throws Exception()
+        every { tasksRepository.deleteTaskById(dummyTask.id) } throws Exception()
 
         // Then& When
         assertThrows<Exception> {
             tasksRepository.deleteTaskById(
-                task.id
+                dummyTask.id
             )
         }
         verify(exactly = 0) {
-            logsRepository.addLog(match {
-                it.affectedId == task.id.toString()
-                        &&
-                        it.affectedType == Log.AffectedType.TASK
+            logsRepository.addLog(
+                match {
+                    it is DeletedLog &&
+                    it.affectedId == dummyTask.id.toString() &&
+                    it.affectedType == Log.AffectedType.TASK
 
 
-            })
+                })
         }
     }
 
 }
-
-private val user = User(
-    id = UUID.randomUUID(),
-    username = "adminUser",
-    hashedPassword = "hashed",
-    role = UserRole.ADMIN,
-    cratedAt = LocalDateTime.now()
-)
-
-
-private val fixedProjectId = UUID.fromString("9f1602cc-87c0-4319-96b5-5d43766b9ae9") // consistent across test
-
-
-private val task = Task(
-    id = UUID.randomUUID(),
-    title = "Task A",
-    state = "todo",
-    assignedTo = listOf(),
-    createdBy = user.id,
-    createdAt = LocalDateTime.now(),
-    projectId = fixedProjectId
-)
