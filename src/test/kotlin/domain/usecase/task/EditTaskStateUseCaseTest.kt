95f240593efe146655396c1a04e0803ef8d975c6
mohamedshemees <mohamedessampd@gmail.com>
2025-05-10 18:54:17 +0300

    test: enhance EditTaskStateUseCaseTest with additional scenarios and error handling

--- src/test/kotlin/domain/usecase/task/EditTaskStateUseCaseTest.kt
+++ src/test/kotlin/domain/usecase/task/EditTaskStateUseCaseTest.kt
@@ -1,10 +1,12 @@
 package domain.usecase.task
 
+import dummyAdmin
 import dummyProject
 import dummyTasks
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import org.example.domain.AccessDeniedException
 import org.example.domain.entity.State
 import org.example.domain.entity.log.ChangedLog
 import org.example.domain.repository.LogsRepository
@@ -22,7 +24,7 @@ class EditTaskStateUseCaseTest {
     private val usersRepository: UsersRepository = mockk(relaxed = true)
     private val tasksRepository: TasksRepository = mockk(relaxed = true)
     private val projectsRepository: ProjectsRepository = mockk(relaxed = true)
-    private val dummyTask = dummyTasks[0]
+
 
     @BeforeEach
     fun setup() {
@@ -37,14 +39,15 @@ class EditTaskStateUseCaseTest {
     @Test
     fun `should edit task state when task exists`() {
         // Given
-        val task = dummyTask.copy(projectId = dummyProject.id, state = State(name = "test-state"))
-        val newState = dummyProject.states.random().name
+        val project = dummyProject.copy(createdBy = dummyAdmin.id)
+        val task = dummyTask.copy(projectId = project.id, state = State(name = "test-state"))
+        val newState = project.states.random().name
+
+        every { usersRepository.getCurrentUser() } returns dummyAdmin
         every { tasksRepository.getTaskById(task.id) } returns task
-        every { projectsRepository.getProjectById(task.projectId) } returns dummyProject
-
+        every { projectsRepository.getProjectById(task.projectId) } returns project
         // When
-        editTaskStateUseCase(dummyTask.id, newState)
-
+        editTaskStateUseCase(task.id, newState)
         // Then
         verify {
             tasksRepository.updateTask(match {
@@ -58,6 +61,64 @@ class EditTaskStateUseCaseTest {
                     it is ChangedLog
                 })
         }
+    }
+
+    @Test
+    fun `should throw AccessDeniedException when project is not created by current user given task id & new state`() {
+        // Given
+        val project = dummyProject
+        val task = dummyTask.copy(projectId = project.id, state = State(name = "test-state"))
+        val newState = project.states.random().name
+
+        every { usersRepository.getCurrentUser() } returns dummyAdmin
+        every { tasksRepository.getTaskById(task.id) } returns task
+        every { projectsRepository.getProjectById(task.projectId) } returns project
+        // When
+        assertThrows<AccessDeniedException> {
+            editTaskStateUseCase(task.id, newState)
+        }
+    }
+
+    @Test
+    fun `should not proceed when getCurrentUser fails`() {
+        //given
+        every { usersRepository.getCurrentUser() } throws Exception()
+        //when && then
+        assertThrows<Exception> {
+            editTaskStateUseCase(dummyProject.id, "new name")
+        }
+        verify(exactly = 0) { tasksRepository.getTaskById(any()) }
+        verify(exactly = 0) { projectsRepository.getProjectById(any()) }
+        verify(exactly = 0) { tasksRepository.updateTask(any()) }
+        verify(exactly = 0) { logsRepository.addLog(any()) }
+    }
+
+    @Test
+    fun `should not complete execution when getTaskById fails`() {
+        //given
+        every { usersRepository.getCurrentUser() } returns dummyAdmin
+        every { tasksRepository.getTaskById(any()) } throws Exception()
+        //when && then
+        assertThrows<Exception> {
+            editTaskStateUseCase(dummyProject.id, "new name")
+        }
+        verify(exactly = 0) { projectsRepository.getProjectById(any()) }
+        verify(exactly = 0) { tasksRepository.updateTask(any()) }
+        verify(exactly = 0) { logsRepository.addLog(any()) }
+    }
+
+    @Test
+    fun `should not complete execution when getProjectById fails`() {
+        //given
+        every { usersRepository.getCurrentUser() } returns dummyAdmin
+        every { tasksRepository.getTaskById(dummyTask.id) } returns dummyTask
+        every { projectsRepository.getProjectById(dummyTask.projectId) } throws Exception()
+        //when && then
+        assertThrows<Exception> {
+            editTaskStateUseCase(dummyTask.id, "new name")
+        }
+        verify(exactly = 0) { tasksRepository.updateTask(any()) }
+        verify(exactly = 0) { logsRepository.addLog(any()) }
     }
 
     @Test
@@ -87,13 +148,15 @@ class EditTaskStateUseCaseTest {
     @Test
     fun `should throw an Exception and not log when updateTask fails `() {
         // Given
-        val task = dummyTask.copy(projectId = dummyProject.id, state = State(name = "test-state"))
+        val project = dummyProject.copy(createdBy = dummyAdmin.id)
+        val task = dummyTask.copy(projectId = project.id, state = State(name = "test-state"))
+        every { usersRepository.getCurrentUser() } returns dummyAdmin
         every { tasksRepository.getTaskById(task.id) } returns task
-        every { projectsRepository.getProjectById(task.projectId) } returns dummyProject
+        every { projectsRepository.getProjectById(task.projectId) } returns project
         every { tasksRepository.updateTask(any()) } throws Exception()
         // when&Then
         assertThrows<Exception> {
-            editTaskStateUseCase(task.id, dummyProject.states.random().name)
+            editTaskStateUseCase(task.id, project.states.random().name)
         }
 
         verify(exactly = 0) {
@@ -105,6 +168,6 @@ class EditTaskStateUseCaseTest {
         }
     }
 
-
+    private val dummyTask = dummyTasks[0]
 }
 
