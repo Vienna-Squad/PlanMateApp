b34da01672adbc1115c8027d1f47e066bd50bad6
Mohannad Ahmed <mohannad199344@gmail.com>
2025-05-10 12:40:24 +0300

    refactor: enhance GetAllTasksOfProjectUseCase with access control and improve test coverage for edge cases

--- src/main/kotlin/di/UseCasesModule.kt
+++ src/main/kotlin/di/UseCasesModule.kt
@@ -12,23 +12,23 @@ val useCasesModule = module {
     single { LogoutUseCase(get()) }
     single { LoginUseCase(get()) }
     single { CreateUserUseCase(get(), get()) }
-    single { AddMateToProjectUseCase(get(), get(),get()) }
-    single { AddStateToProjectUseCase(get(), get(),get()) }
+    single { AddMateToProjectUseCase(get(), get(), get()) }
+    single { AddStateToProjectUseCase(get(), get(), get()) }
     single { CreateProjectUseCase(get(), get(), get()) }
-    single { DeleteMateFromProjectUseCase(get(), get(),get()) }
-    single { DeleteProjectUseCase(get(), get(),get()) }
-    single { DeleteStateFromProjectUseCase(get(), get(),get()) }
-    single { EditProjectNameUseCase(get(), get(),get()) }
-    single { GetAllTasksOfProjectUseCase(get()) }
+    single { DeleteMateFromProjectUseCase(get(), get(), get()) }
+    single { DeleteProjectUseCase(get(), get(), get()) }
+    single { DeleteStateFromProjectUseCase(get(), get(), get()) }
+    single { EditProjectNameUseCase(get(), get(), get()) }
+    single { GetAllTasksOfProjectUseCase(get(), get(), get()) }
     single { GetProjectHistoryUseCase(get()) }
-    single { CreateTaskUseCase(get(), get(), get(),get()) }
+    single { CreateTaskUseCase(get(), get(), get(), get()) }
     single { GetProjectHistoryUseCase(get()) }
-    single { DeleteTaskUseCase(get(), get(),get(),get(),) }
+    single { DeleteTaskUseCase(get(), get(), get(), get()) }
     single { GetTaskHistoryUseCase(get()) }
-    single { GetTaskUseCase(get(),get(),get(),) }
-    single { AddMateToTaskUseCase(get(), get(),get(),get()) }
-    single { DeleteMateFromTaskUseCase(get(), get(),get(),get(),) }
-    single { EditTaskStateUseCase(get(), get(),get(),get()) }
-    single { EditTaskTitleUseCase(get(), get(),get(),get(),) }
+    single { GetTaskUseCase(get(), get(), get()) }
+    single { AddMateToTaskUseCase(get(), get(), get(), get()) }
+    single { DeleteMateFromTaskUseCase(get(), get(), get(), get()) }
+    single { EditTaskStateUseCase(get(), get(), get(), get()) }
+    single { EditTaskTitleUseCase(get(), get(), get(), get()) }
     single { GetAllProjectsUseCase(get(), get()) }
 }
--- src/test/kotlin/domain/usecase/project/GetAllTasksOfProjectUseCaseTest.kt
+++ src/test/kotlin/domain/usecase/project/GetAllTasksOfProjectUseCaseTest.kt
@@ -1,12 +1,18 @@
 package domain.usecase.project
 
 import com.google.common.truth.Truth.assertThat
+import dummyAdmin
+import dummyMate
 import dummyProject
 import dummyTasks
 import io.mockk.every
 import io.mockk.mockk
+import io.mockk.verify
+import org.example.domain.AccessDeniedException
 import org.example.domain.NotFoundException
+import org.example.domain.repository.ProjectsRepository
 import org.example.domain.repository.TasksRepository
+import org.example.domain.repository.UsersRepository
 import org.example.domain.usecase.project.GetAllTasksOfProjectUseCase
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
@@ -16,30 +22,110 @@ class GetAllTasksOfProjectUseCaseTest {
 
     private lateinit var getAllTasksOfProjectUseCase: GetAllTasksOfProjectUseCase
     private val tasksRepository: TasksRepository = mockk(relaxed = true)
+    private val projectsRepository: ProjectsRepository = mockk(relaxed = true)
+    private val usersRepository: UsersRepository = mockk(relaxed = true)
 
     @BeforeEach
     fun setup() {
-        getAllTasksOfProjectUseCase = GetAllTasksOfProjectUseCase(tasksRepository)
+        getAllTasksOfProjectUseCase = GetAllTasksOfProjectUseCase(tasksRepository, projectsRepository, usersRepository)
     }
 
     @Test
-    fun `should return tasks of project when existed project has tasks`() {
+    fun `should return tasks when project creator retrieves tasks`() {
         //given
-        every { tasksRepository.getAllTasks() } returns dummyTasks + dummyTasks.random()
-            .copy(projectId = dummyProject.id) + dummyTasks.random()
-            .copy(projectId = dummyProject.id) + dummyTasks.random().copy(projectId = dummyProject.id)
+        val project = dummyProject.copy(createdBy = dummyAdmin.id)
+        val tasks = dummyTasks + listOf(
+            dummyTasks.random().copy(projectId = project.id),
+            dummyTasks.random().copy(projectId = project.id),
+        )
+        every { usersRepository.getCurrentUser() } returns dummyAdmin
+        every { projectsRepository.getProjectById(project.id) } returns project
+        every { tasksRepository.getAllTasks() } returns tasks
         //when
-        val tasks = getAllTasksOfProjectUseCase(dummyProject.id)
+        val filteredTasks = getAllTasksOfProjectUseCase(project.id)
         //then
-        assertThat(tasks.size).isEqualTo(3)
-        assertThat(tasks.all { it.projectId == dummyProject.id }).isTrue()
+        assertThat(filteredTasks.all { it.projectId == project.id }).isTrue()
     }
 
     @Test
-    fun `should throw NotFoundException when existed project has no tasks`() {
+    fun `should return tasks when project mate retrieves tasks`() {
         //given
+        val project = dummyProject.copy(matesIds = dummyProject.matesIds + dummyMate.id)
+        val tasks = dummyTasks + listOf(
+            dummyTasks.random().copy(projectId = project.id),
+            dummyTasks.random().copy(projectId = project.id),
+        )
+        every { usersRepository.getCurrentUser() } returns dummyMate
+        every { projectsRepository.getProjectById(project.id) } returns project
+        every { tasksRepository.getAllTasks() } returns tasks
+        //when
+        val filteredTasks = getAllTasksOfProjectUseCase(project.id)
+        //then
+        assertThat(filteredTasks.all { it.projectId == project.id }).isTrue()
+    }
+
+    @Test
+    fun `should throw AccessDeniedException when user is not related to its project`() {
+        //given
+        every { usersRepository.getCurrentUser() } returns dummyAdmin
+        every { projectsRepository.getProjectById(dummyProject.id) } returns dummyProject
+        //when && then
+        assertThrows<AccessDeniedException> { getAllTasksOfProjectUseCase(dummyProject.id) }
+        verify(exactly = 0) { tasksRepository.getAllTasks() }
+    }
+
+    @Test
+    fun `should throw NotFoundException when project has no tasks`() {
+        //given
+        val project = dummyProject.copy(createdBy = dummyAdmin.id)
+        every { usersRepository.getCurrentUser() } returns dummyAdmin
+        every { projectsRepository.getProjectById(project.id) } returns project
         every { tasksRepository.getAllTasks() } returns dummyTasks
         //when && then
-        assertThrows<NotFoundException> { getAllTasksOfProjectUseCase(dummyProject.id) }
+        assertThrows<NotFoundException> { getAllTasksOfProjectUseCase(project.id) }
+    }
+
+    @Test
+    fun `should throw NotFoundException when all tasks list is empty`() {
+        //given
+        val project = dummyProject.copy(createdBy = dummyAdmin.id)
+        every { usersRepository.getCurrentUser() } returns dummyAdmin
+        every { projectsRepository.getProjectById(project.id) } returns project
+        every { tasksRepository.getAllTasks() } returns emptyList()
+        //when && then
+        assertThrows<NotFoundException> { getAllTasksOfProjectUseCase(project.id) }
+    }
+
+    @Test
+    fun `should not proceed when getCurrentUser fails`() {
+        //given
+        every { usersRepository.getCurrentUser() } throws Exception()
+        //when && then
+        assertThrows<Exception> { getAllTasksOfProjectUseCase(dummyProject.id) }
+        verify(exactly = 0) { projectsRepository.getProjectById(any()) }
+        verify(exactly = 0) { tasksRepository.getAllTasks() }
+    }
+
+    @Test
+    fun `should not proceed when getProjectById fails`() {
+        //given
+        val project = dummyProject.copy(createdBy = dummyAdmin.id)
+        every { usersRepository.getCurrentUser() } returns dummyAdmin
+        every { projectsRepository.getProjectById(project.id) } throws Exception()
+        //when && then
+        assertThrows<Exception> { getAllTasksOfProjectUseCase(project.id) }
+        verify(exactly = 0) { tasksRepository.getAllTasks() }
+
+    }
+
+    @Test
+    fun `should not proceed when getAllTasks fails`() {
+        //given
+        val project = dummyProject.copy(createdBy = dummyAdmin.id)
+        every { usersRepository.getCurrentUser() } returns dummyAdmin
+        every { projectsRepository.getProjectById(project.id) } returns project
+        every { tasksRepository.getAllTasks() } throws Exception()
+        //when && then
+        assertThrows<Exception> { getAllTasksOfProjectUseCase(project.id) }
     }
 }
